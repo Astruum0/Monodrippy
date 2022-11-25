70adea246803daaa67db8bcdbf810f753ac51e8b
Jules Dupuis <jules.dupuis@ynov.com>
2022-11-25 15:14:31 +0100

    pseudo + id

--- frontend/src/components/JoinRoom.vue
+++ frontend/src/components/JoinRoom.vue
@@ -1,10 +1,15 @@
 <template>
   <div>
+    <div class="input-group">
+      <span class="input-group-text">Ton pseudo</span>
+      <input type="text" id="pseudo" aria-label="Pseudo" class="form-control">
+      <button type="button" @click="getValue">Valider le pseudo</button>
+    </div>
     <div v-for="room in rooms" :key="room">
-      <div class="card card-primary mt-5 container room">
+      <div @click="onClick" :id="room.id" class="card card-primary mt-5 container room" style="cursor: pointer;">
         <div class="row">
           <div class="col-12 col-md-6 my-3">
-            <h1 class="room-name">{{room.name}}</h1>
+            <h1 class="room-name">{{ room.name }}</h1>
             <div v-if="room.locked == true" class="d-flex justify-content-center">
               <p class="mb-0">Partie en cours</p>
             </div>
@@ -14,10 +19,10 @@
             </div>
           </div>
           <div class="col-12 col-md-6 d-none d-md-flex justify-content-end">
-            <span class="room-people my-auto me-3">{{room.people}}/4</span>
+            <span class="room-people my-auto me-3">{{ room.people }}/4</span>
           </div>
           <div class="col-12 col-md-6 d-flex d-md-none justify-content-center">
-            <span class="room-people my-auto ">{{room.people}}/4</span>
+            <span class="room-people my-auto ">{{ room.people }}/4</span>
           </div>
         </div>
       </div>
@@ -29,29 +34,43 @@
 </template>
 
 <script lang="js">
-
+import $ from 'jquery';
 let JoinRoom = {
 
-  data:   function() {
+  data: function () {
     return {
       visible: false,
       rooms: [
         {
+          id: 0,
           name: 'Serveur 1',
           people: 4,
           locked: true,
         },
         {
+          id: 1,
           name: 'Serveur 2',
           people: 2,
           locked: false,
         },
-      ]
+      ],
+      pseudo: "",
+      selected: -1,
     }
   },
 
   methods: {
-  
+    onClick(e) {
+      console.log(e);
+      var select  = $(e.target).closest(".card").attr("id");
+      console.log(select);
+      this.selected = select;
+    },
+    getValue() {
+      // Sélectionner l'élément input et récupérer sa valeur
+      var input = document.getElementById("pseudo").value;
+      this.pseudo = input;
+    }
   }
 }
 
@@ -60,21 +79,27 @@ export default JoinRoom
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-.room-name{
+.room-name {
   color: white;
   text-shadow: 2px 2px 5px #232323;
 }
 
-.room-people{
+.room-people {
   color: white;
   font-size: 3.5rem;
   text-shadow: 2px 2px 5px #232323;
 }
 
 #cercle {
-    width: 10px;
-    height: 10px;
-    border-radius: 20px;
-    background: rgb(5, 224, 5);
+  width: 10px;
+  height: 10px;
+  border-radius: 20px;
+  background: rgb(5, 224, 5);
+}
+
+.input-group-text {
+  background-color: red !important;
+  color: white !important;
+  text-shadow: 2px 2px 5px #232323;
 }
 </style>
