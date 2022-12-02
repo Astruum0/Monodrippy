211c92ee1afead92676870e210712a2bdaeebfde
Mattéo Christophe <matteo.christophe@opquast.com>
2022-12-02 16:50:39 +0100

    updated luck.json + luck service

--- backend/src/luck/luck.controller.ts
+++ backend/src/luck/luck.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { luck } from './luck.schema';
 import { luckService } from './luck.service';
 
@@ -16,5 +16,14 @@ export class luckController {
     @Param('id') id: Number,
   ): Promise<luck> {
     return this.luckService.findById(id);
+  }
+
+
+  @Post(':id/luckaction')
+  async tileAction(@Body() json: String, @Param('id') id: number) {
+    let board_id = json["board_id"]
+    let luck_id = id
+    let player_id = json["player_id"]
+    return await this.luckService.luckAction(board_id, luck_id, player_id);
   }
 }
--- backend/src/luck/luck.service.ts
+++ backend/src/luck/luck.service.ts
@@ -2,10 +2,17 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { luck, luckDocument } from './luck.schema';
+import { board, boardDocument } from "src/board/board.schema";
+import { Action } from "src/models/action";
+import { player, playerDocument } from "src/player/player.schema";
 
 @Injectable()
 export class luckService {
-  constructor(@InjectModel(luck.name) private luckModel: Model<luckDocument>) {}
+  constructor(
+    @InjectModel(luck.name) private luckModel: Model<luckDocument>,
+    @InjectModel(board.name) private boardModel: Model<boardDocument>,
+    @InjectModel(player.name) private playerModel: Model<playerDocument>,
+  ) {}
 
   async findAll(): Promise<luck[]> {
     return this.luckModel.find().exec();
@@ -13,5 +20,28 @@ export class luckService {
 
   async findById(luckId: Number): Promise<luck> {
     return this.luckModel.findOne({"id": luckId}).exec();
+  }
+
+  async luckAction(board_id: number, luck_id: number, player_id: string){
+    let game = await this.boardModel.findOne({
+      id: board_id
+    }).exec();
+    let player = await this.playerModel.findOne({
+      id: player_id
+    }).exec();
+    let index = this.findPlayerIndex(game, player.id)
+    let luck = game["lucks"][luck_id]
+    let effect = luck.cardEffect["effect"]
+    let value = luck.cardEffect["value"]
+    console.log(effect, value)
+  }
+
+
+  findPlayerIndex(game: any, player_id: string) {
+    for (let index = 0; index < game["players"].length; index++) {
+      if (game["players"][index].id == player_id) {
+        return index
+      }
+    }
   }
 }
