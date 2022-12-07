88c6c61a63abf82d9c49c7bce10d874546110475
paul-lautier <paul.lautier64@gmail.Com>
2022-12-07 21:56:40 +0100

    je veux lme foutre en l'air

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,4 +1,12 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
+// const { defineConfig } = require('@vue/cli-service')
+// module.exports = defineConfig({
+//   transpileDependencies: true,
+// })
+
+module.exports = {
   transpileDependencies: true,
-})
+
+  devServer: {
+  allowedHosts: "all"
+  }
+}
